84b0d955bc2ab5f33f167210dcdc4d0c3477880e
Daria Vesela <daria1vesela@gmail.com>
2024-11-04 16:20:44 +0000

    added basic functionality of update order although not yet smooth -> the specification of what a user wants to update will need to be covered in client calls.

--- cafe_project/order.py
+++ cafe_project/order.py
@@ -3,50 +3,58 @@ from typing import List
 import json
 from pathlib import Path
 from pydantic import BaseModel, Field
-from cafe_project.product import product
+from cafe_project.product import Product
 from cafe_project.entity_manager import JSONCRUDBase
 
-@dataclass
+
 class Order(BaseModel):
     date: int
     time: int
     order_id: int
-    products: List[product]
+    courier_id: int
+    products: List[Product]
     progress: int
+
 
 class OrderManager(JSONCRUDBase):
     def __init__(self):
-        json_file = Path('src/main/data/orders.json')
+        json_file = Path(__file__).parent/'data'/'orders.json'
         super().__init__(json_file)
 
+#add validation for duplicates 
+# check the data structure of order - why is it a string?
+#Current writing into file kinda ugly and unreadable
     def create (self, order: Order) -> Order:
         orders = self._read_from_json()
-        orders[order.order_id] = order.__dict__
+        orders[str(order.order_id)] = order.model_dump()
         self._write_to_json(orders)
         return order
     
-    #reads a concrete order
+    #reads a concrete order, currently expects order_id to be a string (because it is created as a string in the json file!!!)
     def read(self, order_id: int) -> Order:
         orders = self._read_from_json()
-        return Order(**orders[order_id])
+        return Order.model_validate_json(orders[order_id])
     
     # returns a list of all orders
+    #works fine, output ugly though
     def list(self)-> list[Order]:
         data=self._read_from_json()
         list_of_orders: list[Order] =[]
         for entity in data.values():
-            list_of_orders.append(Order(**entity))
+            list_of_orders.append(Order.model_validate((entity)))
         
         return list_of_orders
     
     # updates an order
+    # currently expects THE WHOLE ORDER, should probably only want an order_id and than ask what does the user want to update -> but this should probably be handled in the client_calls.py
+
     def update(self, order: Order) -> Order:
             data = self._read_from_json()
             # Delete the old order if it exists
             if str(order.order_id) in data:
                 del data[str(order.order_id)]
             # Add the new order
-            data[str(order.order_id)] = order.__dict__
+            data[str(order.order_id)] = order.model_dump()
             self._write_to_json(data=data)
             return order
     
@@ -56,4 +64,52 @@ class OrderManager(JSONCRUDBase):
         del data[str(order_id)]
         self._write_to_json(data=data)
 
-    
+    # example product     
+    # "2": {
+    #     "product_id": 2,
+    #     "name": "Cake",
+    #     "price": 9.0
+    # },
+
+# #CREATE
+# manager = OrderManager()
+# manager.create(order=Order(date=122024, time=1050, order_id=2, courier_id=1, products=[Product(product_id=2, name='Cake', price=9)], progress=1))
+# print(manager.list())
+
+
+# #READ
+# manager = OrderManager()
+# print(manager.read("2"))
+# #print(manager.list())
+
+
+#UPDATE
+
+manager = OrderManager()
+order = Order(
+    date='122024',
+    time='1050',
+    order_id='2',
+    courier_id=1,
+    products=[
+        Product(
+            product_id=2,
+            name="Cake",
+            price=9.0
+        )
+    ],
+    progress='1'
+)
+
+manager.update(order)
+print(manager.list())
+
+#DELETE
+# order=Order(date='122023', time='1045', order_id='1', products=[Product(product_id=2, name='Cake', price=9)], progress='1',)
+
+# manager = OrderManager()
+# manager.update(
+#     order
+# )
+# print(manager.list())
+
